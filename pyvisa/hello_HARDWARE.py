fdad4dd75e5f2315e9921b3ff682c151e2bed30d
yvesauad <yvesauad@gmail.com>
2020-05-31 19:07:44 +0200

    v1.1.3. Hardware readout sucessfull. Simple but ok

---
+++ pyvisa/hello_HARDWARE.py
@@ -0,0 +1,16 @@
+import sys
+import enum
+sys.path.insert(0, "/home/yvesauad/.local/lib/python2.7/site-packages")
+#print(sys.path)
+import pyvisa
+pyvisa.ResourceManager()
+rm = pyvisa.ResourceManager()
+tl = rm.open_resource('USB0::4883::32882::1907040::0::INSTR')
+#print(rm.list_resources())
+print(tl.query('*IDN?'))
+tl.write('CONF:POW')
+tl.write('INIT')
+print(tl.query('FETCH?'))
+print(tl.query('CONFIGURE?'))
+print(tl.query('SENS:CORR:WAV?'))
+

19fc90a9bf969256952d06ec285fbe5212fa4206
yvesauad <yvesauad@gmail.com>
2020-06-09 19:03:32 +0200

    v1.3.1. Changes on the lab

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import os
 
 setuptools.setup(
     name="yvorsay-instrumentation_laser",
-    version="1.3.0",
+    version="1.3.1",
     author="Yves Auad",
     description="Laser Control",
     url="https://github.com/yvesauad/yvorsay-instrument",

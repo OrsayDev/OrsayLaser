47b6d6ebc54aa69a95dbcc7b7d292d5c4183668b
yvesauad <yvesauad@gmail.com>
2020-11-01 09:47:07 +0100

    v4.8.0. Minor bug correction.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="yvorsay-instrumentation_laser",
-    version="4.7.0",
+    version="4.8.0",
     author="Yves Auad",
     description="Laser Control",
     url="https://github.com/yvesauad/yvorsay-instrument",

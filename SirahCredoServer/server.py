3ef5f5c60849f3f92c3fffa494689f9a41b70797
yvesauadPI <yvesauad@gmail.com>
2020-11-06 14:36:15 +0100

    RP: Trying to fixed. Arduino with bugs.

--- SirahCredoServer/server.py
+++ SirahCredoServer/server.py
@@ -41,7 +41,7 @@ class ServerSirahCredoLaser:
             self.__ps = power_supply.SpectraPhysics('/dev/ttyUSB1')
             self.__pwmeter = [power.TLPowerMeter('USB0::4883::32882::1907040::0::INSTR'),
                               power.TLPowerMeter('USB0::0x1313::0x8072::1908893::INSTR')]
-            self.__ard = ard_vi.Arduino('/dev/ttyACM0')
+            self.__ard = ard.Arduino('/dev/ttyACM0')
             print('***SERVER***: Server Running in Raspberry Pi. Real Laser employed.')
         else:
             self.__sirah = laser_vi.SirahCredoLaser('COM12')

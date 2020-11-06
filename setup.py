31ffe59a1d3159e1cbcf97e144b0a5bae045849c
yvesauad <yvesauad@gmail.com>
2020-11-06 14:08:23 +0100

    v5.2.3. Minor Issue with Arduino. Problem is raspberry reassigning ports.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="yvorsay-instrumentation_laser",
-    version="5.2.2",
+    version="5.2.3",
     author="Yves Auad",
     description="Laser Control",
     url="https://github.com/yvesauad/yvorsay-instrument",

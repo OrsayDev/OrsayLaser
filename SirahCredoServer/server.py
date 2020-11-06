4d8ad27d62f8b5674e8a827022e6620c9def0a26
yvesauad <yvesauad@gmail.com>
2020-11-06 10:22:43 +0100

    v5.1.1. Second attempt to run laser in raspberry pi.

--- SirahCredoServer/server.py
+++ SirahCredoServer/server.py
@@ -37,8 +37,8 @@ class ServerSirahCredoLaser:
             self.__ard = ard.Arduino()
             print('***SERVER***: Server Running in VG Lumiere. Real laser employed.')
         elif SERVER_HOST == '192.168.137.96':
-            self.__sirah = laser.SirahCredoLaser('/dev/ttyUSB1')
-            self.__ps = power_supply.SpectraPhysics('/dev/ttyUSB0')
+            self.__sirah = laser.SirahCredoLaser('/dev/ttyUSB0')
+            self.__ps = power_supply.SpectraPhysics('/dev/ttyUSB1')
             self.__pwmeter = [power.TLPowerMeter('USB0::4883::32882::1907040::0::INSTR'),
                               power.TLPowerMeter('USB0::0x1313::0x8072::1908893::INSTR')]
             self.__ard = ard.Arduino('/dev/ttyACM0')

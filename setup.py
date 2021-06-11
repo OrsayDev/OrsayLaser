e10d684a0e096a9cf37c24fdd79dbaa9d88342a8
yvesauad <yvesauad@gmail.com>
2021-06-11 15:45:50 +0200

    v5.13.4. Testing on Lumiere

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="OrsayLaser",
-    version="5.13.3",
+    version="5.13.4",
     author="Yves Auad",
     description="Laser Control",
     url="https://github.com/yvesauad/yvorsay-instrument",
-    packages=["nionswift_plugin.laser_mod", "nionswift_plugin.server_mod"],
+    packages=["nionswift_plugin.laser_mod", "SirahCredoServer", "SirahCredoServer.virtualInstruments"],
     python_requires='~=3.6',
     install_requires=["pyserial>=3.4", "pyusb>=1.0.2", "PyVISA>=1.10.1", "PyVISA-py>=0.4.1", "nionswift-usim>=0.3.0"]
 )

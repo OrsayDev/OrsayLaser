47c9064913b5692e55ffa800747bf9ba850a4839
yvesauad <yvesauad@gmail.com>
2021-06-17 16:31:40 +0200

    v5.14.2. Updating my setup file for a clean installation.

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
+setup(
     name="OrsayLaser",
-    version="5.14.1",
+    version="5.14.2",
     author="Yves Auad",
     description="Laser Control",
     url="https://github.com/yvesauad/yvorsay-instrument",
-    packages=["nionswift_plugin.laser_mod", "SirahCredoServer", "SirahCredoServer.virtualInstruments"],
-    python_requires='~=3.6',
-    install_requires=["pyserial>=3.4", "pyusb>=1.0.2", "PyVISA>=1.10.1", "PyVISA-py>=0.4.1", "nionswift-usim>=0.3.0"]
+    packages=['nionswift_plugin.laser_mod', 'SirahCredoServer', 'SirahCredoServer.virtualInstruments'],
+    data_files=[('nionswift_plugin/laser_mod/aux_files', [
+                 'nionswift_plugin/laser_mod/aux_files/Pyrromethene_597.npy',
+                 'nionswift_plugin/laser_mod/aux_files/Pyrromethene_597.json',
+        ]), ('nionswift_plugin/laser_mod/aux_files/dif_delays', [
+        'nionswift_plugin/laser_mod/aux_files/dif_delays/50.txt'
+    ]
+             )],
+    python_requires='>=3.8.5',
+    install_requires=["pyserial>=3.4", "pyusb>=1.0.2", "PyVISA>=1.10.1", "PyVISA-py>=0.4.1"]
 )

5f7fd6eb3e00e4f2b88663868a8b12a8cd11331d
yvesauad <yvesauad@gmail.com>
2022-04-29 08:14:54 +0200

    v6.4.2. setup file corrected

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 from setuptools import setup
 
+
 setup(
     name="OrsayLaser",
-    version="6.4.1",
+    version="6.4.2",
     author="Yves Auad",
     description="Laser Control",
     url="https://github.com/yvesauad/yvorsay-instrument",
     packages=['nionswift_plugin.laser_mod', 'SirahCredoServer', 'SirahCredoServer.virtualInstruments'],
-    data_files=[('nionswift_plugin/laser_mod/aux_files', [
-                 'nionswift_plugin/laser_mod/aux_files/Pyrromethene_597.npy',
-                 'nionswift_plugin/laser_mod/aux_files/Pyrromethene_597.json',
-        ]), ('nionswift_plugin/laser_mod/aux_files/dif_delays', [
-        'nionswift_plugin/laser_mod/aux_files/dif_delays/50.txt'
-    ]
-             )],
+    packages_dir = {'nionswift_plugin.laser_mod': 'src/nionswft_plugin/laser_mod'},
+    package_data = {'nionswift_plugin.laser_mod': ['aux_files/dif_delays/*', 'aux_files/*']},
     python_requires='>=3.8.5',
     install_requires=["pyserial>=3.4", "pyusb>=1.0.2", "PyVISA>=1.10.1", "PyVISA-py>=0.4.1"]
 )

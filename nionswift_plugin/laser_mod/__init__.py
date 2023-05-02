831765cfb7e1deb6409cc17f0859b5bd1b7d5a7e
yvesauad <yvesauad@gmail.com>
2023-05-02 13:45:57 +0200

    v6.6.0: Hardware source under nion instrumentation

--- nionswift_plugin/laser_mod/__init__.py
+++ nionswift_plugin/laser_mod/__init__.py
@@ -1,4 +1,4 @@
-from nion.swift.model import HardwareSource
+from nion.instrumentation import HardwareSource
 
 from . import gain_inst
 from . import gain_panel
@@ -7,6 +7,3 @@ def run():
     simpleInstrument=gain_inst.gainDevice()
     HardwareSource.HardwareSourceManager().register_instrument("sgain_controller", simpleInstrument)
     gain_panel.run(simpleInstrument)
-
-
-
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="OrsayLaser",
-    version="6.5.1",
+    version="6.6.0",
     author="Yves Auad",
     description="Laser Control",
     url="https://github.com/yvesauad/yvorsay-instrument",

9e6395197239195f74e8edfded963ce5b499467a
yvesauad <yvesauad@gmail.com>
2025-01-31 15:48:02 +0100

    v0.1 if DLL is not present, panel does not launch. A message error appears for the user

--- nionswift_plugin/laser_mod_nkt/__init__.py
+++ nionswift_plugin/laser_mod_nkt/__init__.py
@@ -1,9 +1,16 @@
 from nion.utils import Registry
+import logging
 
-from . import gain_inst
-from . import gain_panel
+is_ok = False
+try:
+    from . import gain_inst
+    from . import gain_panel
+    is_ok = True
+except FileNotFoundError:
+    logging.info("***LASER NKT***: Probably not found DLL. Laser cannot be initialized.")
 
 def run():
-    simpleInstrument=gain_inst.gainDevice()
-    Registry.register_component(simpleInstrument, {"sgain_controller_nkt"})
-    gain_panel.run(simpleInstrument)
+    if is_ok:
+        simpleInstrument=gain_inst.gainDevice()
+        Registry.register_component(simpleInstrument, {"sgain_controller_nkt"})
+        gain_panel.run(simpleInstrument)
